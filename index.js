f662392fdb993c73a6f79d819b885ffe83341823
Steve Thompson <writetome51@gmail.com>
2020-09-02 15:32:36 -0600

    minor changes

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-import { isFiniteNumber } from '@writetome51/is-finite-number';
-
-
 // if arg ends with .0 (i.e, 1.0), this returns true.
 
-export function isInteger(arg) {
-    return ( isFiniteNumber(arg) && Number.isInteger(arg)); // no errors.
-}
+export const isInteger = Number.isInteger;
 
 
 export function notInteger(arg) {
-    return (!(isInteger(arg)));
+	return (!(isInteger(arg)));
 }
 
 
 // if arg ends with .0 (i.e, 1.0), this returns false.
 
 export function isFloat(arg) {
-    return ( isFiniteNumber(arg) && notInteger(arg));
+	return (Number.isFinite(arg) && notInteger(arg));
 }
